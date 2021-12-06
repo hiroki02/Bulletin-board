94df9a2f55966e2e7e59446583c243cc7a558acb
EC2 Default User <ec2-user@ip-172-31-1-12.us-east-2.compute.internal>
2021-12-06 21:22:05 +0900

    記事作成

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,8 @@
 
 Route::get('/posts', 'PostController@index');
 Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::post('/posts', 'PostController@store');
 Route::get('/posts/{post}', 'PostController@show');
+//
+//一番左がwebのurlの最後の部分　右の部分がpostcontrollerのshowメソッドに飛んでいる(public function show(Post $post))にあたる
